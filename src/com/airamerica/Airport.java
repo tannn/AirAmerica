7a91f9a9ba39e1d5f2c9686ea20469c026f71236
Tanner <tanhusker@gmail.com>
2015-09-21 14:49:01 -0500

    Create airport class, constructor, getters and setters

---
+++ src/com/airamerica/Airport.java
@@ -0,0 +1,58 @@
+package com.airamerica;
+
+public class Airport {
+	
+	private String airportCode;
+	private String airportName;
+	private Address airportAddress;
+	private int airportLatDeg;
+	private int airportLatMin;
+	private int aiportLongDeg;
+	private int airportLongMin;
+	private int passengerFacilityFee;
+	
+	public Airport(String airportCode, String airportName, Address airportAddress, int airportLatDeg, int airportLatMin,
+			int aiportLongDeg, int airportLongMin, int passengerFacilityFee) {
+		this.airportCode = airportCode;
+		this.airportName = airportName;
+		this.airportAddress = airportAddress;
+		this.airportLatDeg = airportLatDeg;
+		this.airportLatMin = airportLatMin;
+		this.aiportLongDeg = aiportLongDeg;
+		this.airportLongMin = airportLongMin;
+		this.passengerFacilityFee = passengerFacilityFee;
+	}
+
+	public String getAirportCode() {
+		return airportCode;
+	}
+
+	public String getAirportName() {
+		return airportName;
+	}
+
+	public Address getAirportAddress() {
+		return airportAddress;
+	}
+
+	public int getAirportLatDeg() {
+		return airportLatDeg;
+	}
+
+	public int getAirportLatMin() {
+		return airportLatMin;
+	}
+
+	public int getAiportLongDeg() {
+		return aiportLongDeg;
+	}
+
+	public int getAirportLongMin() {
+		return airportLongMin;
+	}
+
+	public int getPassengerFacilityFee() {
+		return passengerFacilityFee;
+	}
+
+}
