1ce2be1957d679fd005c8ded9ec989c6a8017882
Tanner <tanhusker@gmail.com>
2015-09-21 19:27:20 -0500

    Add Product constructor, getters and setters

--- src/com/airamerica/Product.java
+++ src/com/airamerica/Product.java
@@ -5,5 +5,17 @@ public class Product {
 	private String productCode;
 	private String productType;
 	
+	public Product(String productCode, String productType) {
+		this.productCode = productCode;
+		this.productType = productType;
+	}
 
+	public String getProductCode() {
+		return productCode;
+	}
+
+	public String getProductType() {
+		return productType;
+	}
+	
 }
