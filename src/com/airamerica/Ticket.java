1fdf49a744c7865b7a266c1ed0c5bdc6d9c9be74
rutebega <esper73@gmail.com>
2015-10-05 21:10:12 -0500

    Update Ticket.java

--- src/com/airamerica/Ticket.java
+++ src/com/airamerica/Ticket.java
@@ -1,26 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.airamerica;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+/**
+ *
+ * @author TMarino
+ */
 public class Ticket extends Product {
-	
-	private String depAirportCode;
+    
+        private String depAirportCode;
 	private String arrAirportCode;
 	private String depTime;
 	private String arrTime;
 	private String flightNo;
 	private String flightClass;
 	private String aircraftType;
+        private String date;
+        private String seat;
+        private Person ticketHolder;
+        private String ticketNote;
 
-	public Ticket(String productCode, String productType, String depAirportCode, String arrAirportCode,
-				  String depTime, String arrTime, String flightNo, String flightClass, String aircraftType) {
-		super(productCode, productType);
-		this.depAirportCode = depAirportCode;
-		this.arrAirportCode = arrAirportCode;
-		this.depTime = depTime;
-		this.arrTime = arrTime;
-		this.flightNo = flightNo;
-		this.flightClass = flightClass;
-		this.aircraftType = aircraftType;
-	}
+    public Ticket(String depAirportCode, String arrAirportCode, String depTime,
+            String arrTime, String flightNo, String flightClass, String aircraftType,
+            String date, String seat, Person ticketHolder, String ticketNote,
+            String productCode, String productType) {
+        super(productCode, productType);
+        this.depAirportCode = depAirportCode;
+        this.arrAirportCode = arrAirportCode;
+        this.depTime = depTime;
+        this.arrTime = arrTime;
+        this.flightNo = flightNo;
+        this.flightClass = flightClass;
+        this.aircraftType = aircraftType;
+        this.date = date;
+        this.seat = seat;
+        this.ticketHolder = ticketHolder;
+        this.ticketNote = ticketNote;
+    }
+
+    public Ticket(String productCode, String productType, String date, String seat,
+            Person ticketHolder, String ticketNote) {
+        super(productCode, productType);
+        this.date = date;
+        this.seat = seat;
+        this.ticketHolder = ticketHolder;
+        this.ticketNote = ticketNote;
+        Scanner productFile = null;
+        try {
+		productFile = new Scanner(new FileReader("data/products.dat"));
+            } catch (FileNotFoundException e) {
+		System.out.println("File not found.");
+            }
+        while(productFile.hasNextLine()) {  
+                String line = productFile.nextLine();
+                String[] productData = line.split(";");
+                if(productCode.equals(productData[0])&&productType.equals(productData[1])){
+                    this.depAirportCode = productData[2];
+                    this.arrAirportCode = productData[3];
+                    this.depTime = productData[4];
+                    this.arrTime = productData[5];
+                    this.flightNo = productData[6];
+                    this.flightClass = productData[7];
+                    this.aircraftType = productData[8];
+                }
+        }
+    }
+        
+    
 
 	public String getDepAirportCode() {
 		return depAirportCode;
@@ -49,5 +101,22 @@ public class Ticket extends Product {
 	public String getAircraftType() {
 		return aircraftType;
 	}
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public Person getTicketHolder() {
+        return ticketHolder;
+    }
+
+    public String getTicketNote() {
+        return ticketNote;
+    }
+        
 
 }
