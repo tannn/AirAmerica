7e160acf5be3a7fa5ca40e67194d3ff503a65648
rutebega <esper73@gmail.com>
2015-10-05 21:08:35 -0500

    Update AwardTicket.java

--- src/com/airamerica/AwardTicket.java
+++ src/com/airamerica/AwardTicket.java
@@ -1,13 +1,26 @@
 package com.airamerica;
 
-public class AwardTicket extends Ticket {
+/**
+ *
+ * @author TMarino
+ */
+public class AwardTicket extends Ticket{
 	
 	private String pointsPerMile;
 
-	public AwardTicket(String productCode, String productType, String depAirportCode, String arrAirportCode,
-			String depTime, String arrTime, String flightNo, String flightClass, String aircraftType, String pointsPerMile) {
-		super(productCode, productType, depAirportCode, arrAirportCode, depTime, arrTime, flightNo, flightClass, aircraftType);
+	public AwardTicket(String depAirportCode, String arrAirportCode,
+			String depTime, String arrTime, String flightNo, String flightClass,
+                        String aircraftType, String date, String seat, Person ticketHolder,
+                        String ticketNote, String productCode,String pointsPerMile) {
+		super(depAirportCode,arrAirportCode,depTime,arrTime,flightNo,flightClass,
+                        aircraftType,date,seat,ticketHolder,ticketNote,productCode, "TA");
 		this.pointsPerMile = pointsPerMile;
+	}
+        
+        public AwardTicket(String productCode, String pointsPerMile, String date, String seat,
+                Person ticketHolder, String ticketNote) {
+		super(productCode,"TO",date,seat,ticketHolder,ticketNote);
+                this.pointsPerMile = pointsPerMile;
 	}
 
 	public String getPointsPerMile() {
