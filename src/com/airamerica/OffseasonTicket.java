bd1e48653dd7881b5515163b45e5dafda6acd450
rutebega <esper73@gmail.com>
2015-10-05 21:09:23 -0500

    Update OffseasonTicket.java

--- src/com/airamerica/OffseasonTicket.java
+++ src/com/airamerica/OffseasonTicket.java
@@ -1,15 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.airamerica;
 
-public class OffseasonTicket extends Ticket {
+/**
+ *
+ * @author TMarino
+ */
+public class OffseasonTicket extends Ticket{
 	
 	private String seasonStartDate;
 	private String seasonEndDate;
 	private String rebate;
 
-	public OffseasonTicket(String productCode, String productType, String seasonStartDate, String seasonEndDate, String depAirportCode, String arrAirportCode,
-			String depTime, String arrTime, String flightNo, String flightClass, String aircraftType, String rebate) {
-		super(productCode, productType, depAirportCode, arrAirportCode, depTime, arrTime, flightNo, flightClass, aircraftType);
-		
+	public OffseasonTicket(String productCode, String seasonStartDate,
+                String seasonEndDate, String depAirportCode, String arrAirportCode,
+		String depTime, String arrTime, String flightNo, String flightClass,
+                String aircraftType, String rebate, String date, String seat,
+                Person ticketHolder, String ticketNote) {
+		super(depAirportCode,arrAirportCode,depTime,arrTime,flightNo,flightClass,
+                        aircraftType,date,seat,ticketHolder,ticketNote,productCode,"TO");
+                this.rebate = rebate;
+                this.seasonEndDate = seasonEndDate;
+                this.seasonStartDate = seasonStartDate;
+	}
+        
+        public OffseasonTicket(String productCode, String seasonStartDate,
+                String seasonEndDate, String rebate, String date, String seat,
+                Person ticketHolder, String ticketNote) {
+		super(productCode, "TO", date,seat,ticketHolder,ticketNote);
+                this.rebate = rebate;
+                this.seasonEndDate = seasonEndDate;
+                this.seasonStartDate = seasonStartDate;
 	}
 
 	public String getSeasonStartDate() {
