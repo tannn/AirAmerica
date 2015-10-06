bd5a97b0f9516ab5e5821cefb7aaad02c1dafdb7
rutebega <esper73@gmail.com>
2015-10-05 21:05:04 -0500

    Update Insurance.java

--- src/com/airamerica/Insurance.java
+++ src/com/airamerica/Insurance.java
@@ -1,17 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.airamerica;
 
-public class Insurance extends Services {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
-	private String name;
+/**
+ *
+ * @author EPiquette
+ */
+public class Insurance extends Service{
+        
+        private String name;
 	private String ageClass;
 	private String costPerMile;
+        private int quantity;
+        private String ticketCode;
 
-	public Insurance(String productCode, String productType, String name, String ageClass, String costPerMile) {
-		super(productCode, productType);
-		this.name = name;
-		this.ageClass = ageClass;
-		this.costPerMile = costPerMile;
-	}
+    public Insurance(String name, String ageClass, String costPerMile, int quantity,
+            String ticketCode, String productCode) {
+        super(productCode, "SI");
+        this.name = name;
+        this.ageClass = ageClass;
+        this.costPerMile = costPerMile;
+        this.quantity = quantity;
+        this.ticketCode = ticketCode;
+    }
+
+    public Insurance(String productCode, int quantity, String ticketCode) {
+        super(productCode, "SI");
+        this.quantity = quantity;
+        this.ticketCode = ticketCode;
+        Scanner productFile = null;
+        try {
+		productFile = new Scanner(new FileReader("data/products.dat"));
+            } catch (FileNotFoundException e) {
+		System.out.println("File not found.");
+            }
+        while(productFile.hasNextLine()) {  
+                String line = productFile.nextLine();
+                String[] productData = line.split(";");
+                if(productCode.equals(productData[0])&&"SI".equals(productData[1])){
+                    this.name = productData[2];
+                    this.ageClass = productData[3];
+                    this.costPerMile = productData[4];
+                }
+        }
+    }
+    
+    
 
 	public String getName() {
 		return name;
@@ -24,5 +64,5 @@ public class Insurance extends Services {
 	public String getCostPerMile() {
 		return costPerMile;
 	}
-
+    
 }
