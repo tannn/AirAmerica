2fa3f5b862a395c561000b903e4c4d7b23ef34cb
rutebega <esper73@gmail.com>
2015-10-05 21:05:50 -0500

    Update Refreshments.java

--- src/com/airamerica/Refreshments.java
+++ src/com/airamerica/Refreshments.java
@@ -1,15 +1,44 @@
 package com.airamerica;
 
-public class Refreshments extends Services {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
+/**
+ *
+ * @author EPiquette
+ */
+public class Refreshments extends Service {
+    
+    
 	private String name;
 	private String cost;
+        private int quantity;
 
-	public Refreshments(String productCode, String productType, String name, String cost) {
-		super(productCode, productType);
+	public Refreshments(String productCode, String name, String cost, int quantity) {
+		super(productCode, "SR");
 		this.name = name;
 		this.cost = cost;
+                this.quantity = quantity;
 	}
+
+    public Refreshments(String productCode, int quantity) {
+        super(productCode, "SR");
+        Scanner productFile = null;
+        try {
+		productFile = new Scanner(new FileReader("data/products.dat"));
+            } catch (FileNotFoundException e) {
+		System.out.println("File not found.");
+            }
+        while(productFile.hasNextLine()) {  
+                String line = productFile.nextLine();
+                String[] productData = line.split(";");
+                if(productCode.equals(productData[0])&&"SR".equals(productData[1])){
+                    this.name = productData[2];
+                    this.cost = productData[3];
+                }
+        }
+    }
 
 	public String getName() {
 		return name;
@@ -18,5 +47,5 @@ public class Refreshments extends Services {
 	public String getCost() {
 		return cost;
 	}
-
+    
 }
