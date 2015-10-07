eb77f53e20223c993379e27666fb475b61193621
Tanner <tannn@users.noreply.github.com>
2015-10-06 23:15:37 -0500

    Update Insurance.java

--- src/com/airamerica/Insurance.java
+++ src/com/airamerica/Insurance.java
@@ -12,46 +12,44 @@ import java.util.Scanner;
  *
  * @author EPiquette
  */
-public class Insurance extends Services{
-        
-        private String name;
+public class Insurance extends Services {
+
+	private String name;
 	private String ageClass;
 	private String costPerMile;
-        private int quantity;
-        private String ticketCode;
+	private int quantity;
+	private String ticketCode;
 
-    public Insurance(String name, String ageClass, String costPerMile, int quantity,
-            String ticketCode, String productCode) {
-        super(productCode, "SI");
-        this.name = name;
-        this.ageClass = ageClass;
-        this.costPerMile = costPerMile;
-        this.quantity = quantity;
-        this.ticketCode = ticketCode;
-    }
+	public Insurance(String name, String ageClass, String costPerMile, int quantity, String ticketCode,
+			String productCode) {
+		super(productCode, "SI");
+		this.name = name;
+		this.ageClass = ageClass;
+		this.costPerMile = costPerMile;
+		this.quantity = quantity;
+		this.ticketCode = ticketCode;
+	}
 
-    public Insurance(String productCode, int quantity, String ticketCode) {
-        super(productCode, "SI");
-        this.quantity = quantity;
-        this.ticketCode = ticketCode;
-        Scanner productFile = null;
-        try {
-		productFile = new Scanner(new FileReader("data/products.dat"));
-            } catch (FileNotFoundException e) {
-		System.out.println("File not found.");
-            }
-        while(productFile.hasNextLine()) {  
-                String line = productFile.nextLine();
-                String[] productData = line.split(";");
-                if(productCode.equals(productData[0])&&"SI".equals(productData[1])){
-                    this.name = productData[2];
-                    this.ageClass = productData[3];
-                    this.costPerMile = productData[4];
-                }
-        }
-    }
-    
-    
+	public Insurance(String productCode, int quantity, String ticketCode) {
+		super(productCode, "SI");
+		this.quantity = quantity;
+		this.ticketCode = ticketCode;
+		Scanner productFile = null;
+		try {
+			productFile = new Scanner(new FileReader("data/products.dat"));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
+		while (productFile.hasNextLine()) {
+			String line = productFile.nextLine();
+			String[] productData = line.split(";");
+			if (productCode.equals(productData[0]) && "SI".equals(productData[1])) {
+				this.name = productData[2];
+				this.ageClass = productData[3];
+				this.costPerMile = productData[4];
+			}
+		}
+	}
 
 	public String getName() {
 		return name;
@@ -64,5 +62,5 @@ public class Insurance extends Services{
 	public String getCostPerMile() {
 		return costPerMile;
 	}
-    
+
 }
