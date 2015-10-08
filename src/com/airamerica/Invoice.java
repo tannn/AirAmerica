f810fd6de6c541c0f8dd1724867a8e113d972a50
rutebega <esper73@gmail.com>
2015-10-08 16:45:00 -0500

    Update Invoice.java

--- src/com/airamerica/Invoice.java
+++ src/com/airamerica/Invoice.java
@@ -26,21 +26,27 @@ public class Invoice {
 		for (String s : productInfo) {
 			String[] productInvoice = s.split(":");
 			if (Product.getProductType(productInvoice[0]).equals("TS")) {
-				products.add(new Ticket(
-						//Why skip productInvoice[2]? This causes a NullPointerException
-						productInvoice[0], "TS", productInvoice[1], productInvoice[3], new Person(productInvoice[4],
-								productInvoice[5], Integer.parseInt(productInvoice[6]), productInvoice[7]),
-						productInvoice[8]));
+				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
+                        ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
+                                Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
+                    }
+                    products.add(new Ticket(productInvoice[0], "TS", productInvoice[1],
+                            productInvoice[3], ticketHolders,
+                            productInvoice[productInvoice.length - 1]));
 			} else if (Product.getProductType(productInvoice[0]).equals("TA")) {
-				products.add(new AwardTicket(
-						productInvoice[0], null, productInvoice[1], productInvoice[3], new Person(productInvoice[4],
-								productInvoice[5], Integer.parseInt(productInvoice[6]), productInvoice[7]),
-						productInvoice[8]));
+				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
+                        ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
+                                Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
+                    }
+                    products.add(new AwardTicket(productInvoice[0], null, productInvoice[1],
+                            productInvoice[3], ticketHolders, productInvoice[productInvoice.length - 1]));
 			} else if (Product.getProductType(productInvoice[0]).equals("TO")) {
-				products.add(new OffseasonTicket(productInvoice[0], null,
-						null, null, productInvoice[1], productInvoice[3], new Person(productInvoice[4],
-								productInvoice[5], Integer.parseInt(productInvoice[6]), productInvoice[7]),
-						productInvoice[8]));
+				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
+                        ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
+                                Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
+                    }
+                    products.add(new OffseasonTicket(productInvoice[0], null, null, null,
+                            productInvoice[1], productInvoice[3], ticketHolders, productInvoice[productInvoice.length - 1]));
 			} else if (Product.getProductType(productInvoice[0]).equals("SI")) {
 				products.add(new Insurance(productInvoice[0], Integer.parseInt(productInvoice[1]), productInvoice[2]));
 			} else if (Product.getProductType(productInvoice[0]).equals("SS")) {
