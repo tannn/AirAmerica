303d0a717cfd2bd52f72195cb1c9812b3410ffb7
rutebega <esper73@gmail.com>
2015-10-08 16:59:58 -0500

    Update Airport.java

--- src/com/airamerica/Airport.java
+++ src/com/airamerica/Airport.java
@@ -22,6 +22,28 @@ public class Airport {
 		this.airportLongMin = airportLongMin;
 		this.passengerFacilityFee = passengerFacilityFee;
 	}
+	
+	public static Airport getAirport(String airportCode){
+            Scanner airportFile = null;
+            try {
+		airportFile = new Scanner(new FileReader("data/airports.dat"));
+            } catch (FileNotFoundException e) {
+		System.out.println("File not found.");
+            }
+            while(airportFile.hasNextLine()) {  
+                String line = airportFile.nextLine();
+                String[] airportData = line.split(";");
+                if(airportCode.equals(airportData[0])){
+                    String[] address = airportData[2].split(",");
+                    return new Airport(airportData[0], airportData[1], 
+                            new Address(address[0], address[1], address[2], address[3], address[5]),
+                            Integer.parseInt(airportData[3]), Integer.parseInt(airportData[4]),
+                            Integer.parseInt(airportData[5]), Integer.parseInt(airportData[6]),
+                            Float.parseFloat(airportData[7]));
+                }
+        }
+        return null;
+        }
 
 	public String getAirportCode() {
 		return airportCode;
