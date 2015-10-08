809c63d27e19ef41df0659d2ecae0b5b6f67646d
Tanner <tannn@users.noreply.github.com>
2015-10-07 20:51:27 -0500

    Update Product.java

--- src/com/airamerica/Product.java
+++ src/com/airamerica/Product.java
@@ -4,11 +4,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class Product {
-	
+abstract public class Product {
+	//calculate price, polymorphism
 	private String productCode;
 	private String productType;
-	
+
 	public Product(String productCode, String productType) {
 		this.productCode = productCode;
 		this.productType = productType;
@@ -21,21 +21,26 @@ public class Product {
 	public String getProductType() {
 		return productType;
 	}
-        
-        public static String getProductType(String code){
-            Scanner productFile = null;
-            try {
-		productFile = new Scanner(new FileReader("data/products.dat"));
-            } catch (FileNotFoundException e) {
-		System.out.println("File not found.");
-            }
-            while(productFile.hasNextLine()) {  
-                String line = productFile.nextLine();
-                String[] productData = line.split(";");
-                if(productData[0].equals(code))
-                    return productData[1];
-            }
-            return null;
-        }
-	
+
+	/**
+	 * @param code Code of product
+	 * @return	Type of product
+	 */
+	public static String getProductType(String code) {
+		Scanner productFile = null;
+		try {
+			productFile = new Scanner(new FileReader("data/Products.dat"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Products.dat not found.");
+		}
+		while (productFile.hasNextLine()) {
+			String line = productFile.nextLine();
+			String[] productData = line.split(";");
+			if (productData[0].equals(code)) {
+				return productData[1];
+			}
+		}
+		return null;
+	}
+
 }
