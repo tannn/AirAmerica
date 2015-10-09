b976720208dcf903f0b99188a54f52cba726ba64
rutebega <esper73@gmail.com>
2015-10-08 21:39:47 -0500

    Update OffseasonTicket.java

--- src/com/airamerica/OffseasonTicket.java
+++ src/com/airamerica/OffseasonTicket.java
@@ -14,17 +14,25 @@ public class OffseasonTicket extends Ticket{
 	private String seasonEndDate;
 	private String rebate;
 
-	public OffseasonTicket(String productCode, String seasonStartDate,
-                String seasonEndDate, String depAirportCode, String arrAirportCode,
-		String depTime, String arrTime, String flightNo, String flightClass,
-                String aircraftType, String rebate, String date, String seat,
-                Person ticketHolder, String ticketNote) {
-		super(depAirportCode,arrAirportCode,depTime,arrTime,flightNo,flightClass,
-                        aircraftType,date,seat,ticketHolder,ticketNote,productCode,"TO");
-                this.rebate = rebate;
-                this.seasonEndDate = seasonEndDate;
-                this.seasonStartDate = seasonStartDate;
-	}
+        public OffseasonTicket(String productCode, String date, String seat,
+                ArrayList<Person> ticketHolders, String ticketNote) {
+		super(productCode, "TO", date,seat,ticketHolders,ticketNote);
+                Scanner productFile = null;
+        try {
+		productFile = new Scanner(new FileReader("data/products.dat"));
+            } catch (FileNotFoundException e) {
+		System.out.println("File not found.");
+            }
+        while(productFile.hasNextLine()) {  
+                String line = productFile.nextLine();
+                String[] productData = line.split(";");
+                if(productCode.equals(productData[0])&&"TO".equals(productData[1])){
+                    this.rebate = productData[2];
+                    this.seasonEndDate = productData[3];
+                    this.seasonStartDate = productData[11];
+                }
+        }
+    }
         
         public OffseasonTicket(String productCode, String seasonStartDate,
                 String seasonEndDate, String rebate, String date, String seat,
