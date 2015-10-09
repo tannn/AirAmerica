8df90947da28a265252a2b8b453be09216de05ab
Tanner <tannn@users.noreply.github.com>
2015-10-09 02:43:09 -0500

    Update SpecialAssistance.java

--- src/com/airamerica/SpecialAssistance.java
+++ src/com/airamerica/SpecialAssistance.java
@@ -1,5 +1,9 @@
 package com.airamerica;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author EPiquette
@@ -15,6 +19,33 @@ public class SpecialAssistance extends Services{
 
 	public String getTypeOfService() {
 		return typeOfService;
+	}
+
+	@Override
+	public double calculatePrice() {
+		return 0;
+	}
+
+	@Override
+	public double calculateTax() {
+		return 0;
+	}
+	
+	public String getReason() {
+		Scanner productFile = null;
+		try {
+			productFile = new Scanner(new FileReader("data/Products.dat"));
+		} catch (FileNotFoundException e) {
+			System.out.println("Products.dat not found.");
+		}
+		while (productFile.hasNextLine()) {
+			String line = productFile.nextLine();
+			String[] productData = line.split(";");
+			if (getProductCode().equals(productData[0]) && "SS".equals(productData[1])) {
+				return productData[2];
+			}
+		}
+		return null;
 	}
     
 }
