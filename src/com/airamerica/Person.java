36cb81680afb163aee58c56f74e15cffc02888cf
rutebega <esper73@gmail.com>
2015-10-09 00:30:46 -0500

    Update Person.java

--- src/com/airamerica/Person.java
+++ src/com/airamerica/Person.java
@@ -1,87 +1,116 @@
 package com.airamerica;
-
+/*
+/* A partial implementation representing a 
+ * Person */
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Person {
+	
+    private String personCode;
+    private Address address;
+    private ArrayList<String> emails;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private int age;
+    private String nationality;
+    private String identityNumber;
 
-	private String personCode;
-	private Address address;
-	private List<String> emails;
-	private String firstName;
-	private String lastName;
-	private String phoneNumber;
-	private int age;
-	private String nationality;
-	private String identityNumber;
-
-	public Person(String personCode, Address address, String firstName, String lastName, String phoneNumber, int age,
-			String nationality, String identityNumber) {
-		this.personCode = personCode;
-		this.address = address;
-		this.emails = new ArrayList<String>();
-		this.firstName = firstName.replaceAll("\\s", "");
-		this.lastName = lastName;
-		this.phoneNumber = phoneNumber;
-		this.age = age;
-		this.nationality = nationality;
-		this.identityNumber = identityNumber;
+    // TODO: Add appropriate constructor(s)
+    public Person(String personCode, Address address, String firstName, String lastName,
+            String phoneNumber, int age, String nationality, String identityNumber) {
+        this.personCode = personCode;
+        this.address = address;
+        this.emails = new ArrayList<String>();
+        this.firstName = firstName.replaceAll("\\s","");
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.nationality = nationality;
+        this.identityNumber = identityNumber;
+    }
+    
+    public Person(String personCode, String identityNumber, int age, String nationality) {
+        this.personCode = personCode;
+        this.identityNumber = identityNumber;
+        this.age = age;
+        this.nationality = nationality;
+        Scanner personFile = null;
+		try {
+			personFile = new Scanner(new FileReader("data/Persons.dat"));
+		} catch (FileNotFoundException e) {
+			System.out.println("data/Persons.dat not found.");
+		}
+		while (personFile.hasNextLine()) {
+			String line = personFile.nextLine();
+			String[] personData = line.split(";");
+			if (personData[0].equals(personCode)) {
+                            String[] name = personData[1].split(",");
+                            this.lastName = name[0];
+                            this.firstName = name[1];
+                            String[] add = personData[2].split(",");
+                            this.address = new Address(add[0], add[1], add[2], add[3], add[4]);
+                            this.phoneNumber = personData[3];
+                            if(personData.length>5)
+                            this.emails.addAll(Arrays.asList(personData[4].split(",")));
+                        }
+		}
 	}
+    
+    public Person(String personCode, Address address) {
+        this.personCode = personCode;
+        this.address = address;
+        this.emails = new ArrayList<String>(); 
+    }
 
-	public Person(String personCode, String identityNumber, int age, String nationality) {
+    // TODO: Add Getters and setters as appropriate
+    public Address getAddress() {
+        return this.address;
+    }
 
-	}
+    public String getPersonCode() {
+        return personCode;
+    }
 
-	public Person(String personCode, Address address) {
-		this.personCode = personCode;
-		this.address = address;
-		this.emails = new ArrayList<String>();
-	}
+    public void setEmails(ArrayList<String> emails) {
+        this.emails = emails;
+    }
 
-	public Address getAddress() {
-		return this.address;
-	}
+    // TODO: Add additional methods here
+    public void addEmail(String email) {
+        this.emails.add(email);
+    }
 
-	public String getPersonCode() {
-		return personCode;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setEmails(List<String> emails) {
-		this.emails = emails;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void addEmail(String email) {
-		this.emails.add(email);
-	}
+    public int getAge() {
+        return age;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getIdentityNumber() {
+        return identityNumber;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getNationality() {
+        return nationality;
+    }
 
-	public int getAge() {
-		return age;
-	}
-
-	public String getIdentityNumber() {
-		return identityNumber;
-	}
-
-	public String getNationality() {
-		return nationality;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	/**
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    
+    /**
 	 * 
 	 * @param code Code of person
 	 * @return Person's name (Last, First)
@@ -108,7 +137,7 @@ public class Person {
 	 * @param code Code of person
 	 * @return	Name (Last, First), Address, and other location identifiers
 	 */
-	public static String getContactInfo(String code) {
+	public static String getContactInfo(String personCode) {
 		Scanner personFile = null;
 		try {
 			personFile = new Scanner(new FileReader("data/Persons.dat"));
@@ -118,11 +147,12 @@ public class Person {
 		while (personFile.hasNextLine()) {
 			String line = personFile.nextLine();
 			String[] personData = line.split(";");
-			if (personData[0].equals(code)) {
-				return personData[1] + "\n\t" + personData[2] + "\n\t" + personData[3] + "," + personData[4] + "," + personData[5] + ","  + personData[6];
+			if (personData[0].equals(personCode)) {
+				return personData[1] + "\n\t" + personData[2] + "\n\t" + personData[3] + "," + personData[4];
 			}
 		}
 		return null;
 	}
-
+    
+	
 }
