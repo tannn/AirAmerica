c055313cdf8a895615d0e2474bd1613542d0716e
Tanner <tannn@users.noreply.github.com>
2015-10-08 23:22:35 -0500

    Update Invoice.java

--- src/com/airamerica/Invoice.java
+++ src/com/airamerica/Invoice.java
@@ -1,9 +1,6 @@
 package com.airamerica;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
  *
@@ -24,30 +21,31 @@ public class Invoice {
 		this.salespersonCode = salespersonCode;
 		this.invoiceDate = invoiceDate;
 		products = new ArrayList<Product>();
+		ArrayList<Person> ticketHolders = new ArrayList<Person>();
+		
 		for (String s : productInfo) {
 			String[] productInvoice = s.split(":");
-			if (Product.getProductType(productInvoice[0]).equals("TS")) {
+			if (Product.getProductType(productInvoice[0].trim()).equals("TS")) {
 				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
-                        ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
-                                Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
-                    }
-                    products.add(new Ticket(productInvoice[0], "TS", productInvoice[1],
-                            productInvoice[3], ticketHolders,
-                            productInvoice[productInvoice.length - 1]));
+					ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
+							Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
+				}
+				products.add(new Ticket(productInvoice[0], "TS", productInvoice[1], productInvoice[3], ticketHolders,
+						productInvoice[productInvoice.length - 1]));
 			} else if (Product.getProductType(productInvoice[0]).equals("TA")) {
 				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
-                        ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
-                                Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
-                    }
-                     products.add(new AwardTicket(productInvoice[0], productInvoice[1],
-                            productInvoice[3], ticketHolders, productInvoice[productInvoice.length - 1]));
+					ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
+							Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
+				}
+				products.add(new AwardTicket(productInvoice[0], productInvoice[1], productInvoice[3],
+						ticketHolders, productInvoice[productInvoice.length - 1]));
 			} else if (Product.getProductType(productInvoice[0]).equals("TO")) {
 				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
-                        ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
-                                Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
-                    }
-                    products.add(new OffseasonTicket(productInvoice[0],
-                            productInvoice[1], productInvoice[3], ticketHolders, productInvoice[productInvoice.length - 1]));
+					ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
+							Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
+				}
+				products.add(new OffseasonTicket(productInvoice[0], productInvoice[1],
+						productInvoice[3], ticketHolders, productInvoice[productInvoice.length - 1]));
 			} else if (Product.getProductType(productInvoice[0]).equals("SI")) {
 				products.add(new Insurance(productInvoice[0], Integer.parseInt(productInvoice[1]), productInvoice[2]));
 			} else if (Product.getProductType(productInvoice[0]).equals("SS")) {
@@ -92,5 +90,39 @@ public class Invoice {
 
 	public ArrayList<Product> getProducts() {
 		return products;
+	}
+	
+	public double getSubtotals() {
+		float total = 0;
+		for (Product p : products) {
+			total += p.calculatePrice();
+		}
+		return total;
+	}
+	
+	public double getFees() {
+		if (Customer.getCustomerType(getCustomerCode()).equals("[CORPORATE]")) 
+			return 40.00;
+		else
+			return 0.00;
+	}
+	
+	public double getTaxes() {
+		float total = 0;
+		for (Product p : products) {
+			total += p.calculateTax();
+		}
+		return total;
+	}
+	
+	public double getDiscounts() {
+		if (Customer.getCustomerType(getCustomerCode()).equals("[CORPORATE]")) 
+			return getSubtotals()  * .12;
+		else
+			return 0.00;
+	}
+	
+	public double getTotals() {
+		return getSubtotals() + getFees() + getTaxes() - getDiscounts();
 	}
 }
