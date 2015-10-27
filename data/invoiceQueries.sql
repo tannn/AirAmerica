f1357df4fef423d458b86c3f15b768e951180b00
rutebega <esper73@gmail.com>
2015-10-26 23:08:10 -0500

    Update invoiceQueries.sql

--- data/invoiceQueries.sql
+++ data/invoiceQueries.sql
@@ -5,7 +5,7 @@ UPDATE Airport JOIN Address ON Airport.address_ID = Address.Address_ID SET Addre
 /*4.*/
 DELETE FROM Airport WHERE Airport_ID = 'HOC';
 /*5.*/
-SELECT * FROM CheckedBaggage JOIN AwardTicket JOIN Refreshment JOIN SpecialAssistance JOIN Insurance JOIN OffseasonTicket JOIN StandardTicket ON CheckedBaggage.InvoiceID = AwardTicket.InvoiceID AND AwardTicket.Invoice_ID = Refreshment.Invoice_ID AND Refreshment.Invoice_ID = SpecialAssistance.Invoice_ID AND SpecialAssistance.Invoice_ID = Insurance.Invoice_ID AND Insurance.Invoice_ID = OffseasonTicket.Invoice_ID AND OffseasonTicket.Invoice_ID = StandardTicket.Invoice_ID WHERE Invoice_ID = 0;
+SELECT * FROM CheckedBaggage JOIN AwardTicket JOIN Refreshment JOIN SpecialAssistance JOIN Insurance JOIN OffseasonTicket JOIN StandardTicket ON CheckedBaggage.Invoice_ID = AwardTicket.Invoice_ID AND AwardTicket.Invoice_ID = Refreshment.Invoice_ID AND Refreshment.Invoice_ID = SpecialAssistance.Invoice_ID AND SpecialAssistance.Invoice_ID = Insurance.Invoice_ID AND Insurance.Invoice_ID = OffseasonTicket.Invoice_ID AND OffseasonTicket.Invoice_ID = StandardTicket.Invoice_ID WHERE Invoice_ID = 0;
 /*6.*/
 SELECT * FROM Invoices WHERE Customer_ID = 7;
 /*7.*/
@@ -15,6 +15,6 @@ SELECT SUM(PointsPerMile) FROM AwardTicket;
 /*9.*/
 SELECT COUNT(*) FROM AwardTickets WHERE Date = '2013/04/16';
 /*10.*/
-SELECT PersonLastName, PersonFirstName, Address, City, State, PhoneNumber, Email, Nationality, Age, PersonCode FROM Person JOIN Passenger JOIN Email JOIN FlightInfo ON Person.Person_ID = Passenger.Person_ID AND Person.Person_ID = Email.Person_ID AND Passenger.Ticket_ID = FlightInfo.Ticket_ID WHERE FlightCode = 'ABCDEF' AND DepartureCity = 'Milwaukee' AND DepartureTime = '07:15'
+SELECT PersonLastName, PersonFirstName, Address, City, State, PhoneNumber, Email, Nationality, Age, PersonCode FROM Person JOIN Passenger JOIN Email JOIN FlightInfo ON Person.Person_ID = Passenger.Person_ID AND Person.Person_ID = Email.Person_ID AND Passenger.Ticket_ID = FlightInfo.Ticket_ID WHERE FlightCode = 'ABCDEF' AND DepartureCity = 'Milwaukee' AND DepartureTime = '07:15';
 /*14.*/
 SELECT * FROM Invoice WHERE Invoice_ID = (SELECT Invoice_ID FROM FlightInfo JOIN Invoice JOIN AwardTicket ON AwardTicket.Invoice_ID = Invoice.Invoice_ID AND FlightInfo.Ticket_ID = AwardTicket.AwardTicket_ID HAVING COUNT(FlightCode)>1) OR Invoice_ID = (SELECT Invoice_ID FROM FlightInfo JOIN Invoice JOIN StandardTicket ON StandardTicket.Invoice_ID = Invoice.Invoice_ID AND FlightInfo.Ticket_ID = StandardTicket.StandardTicket_ID HAVING COUNT(FlightCode)>1) OR Invoice_ID = (SELECT Invoice_ID FROM FlightInfo JOIN Invoice JOIN OffseasonTicket ON OffseasonTicket.Invoice_ID = Invoice.Invoice_ID AND FlightInfo.Ticket_ID = OffseasonTicket.OffseasonTicket_ID HAVING COUNT(FlightCode)>1);
