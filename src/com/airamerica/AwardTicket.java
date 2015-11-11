d68c5af073faa6879222b50632627300977d94c3
Tanner <tannn@users.noreply.github.com>
2015-11-10 22:31:00 -0600

    Update AwardTicket.java

--- src/com/airamerica/AwardTicket.java
+++ src/com/airamerica/AwardTicket.java
@@ -1,9 +1,11 @@
 package com.airamerica;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Scanner;
+
+import com.airamerica.utils.DatabaseInfo;
 
 /**
  *
@@ -11,31 +13,32 @@ import java.util.Scanner;
  */
 public class AwardTicket extends Ticket {
 
-	private String pointsPerMile;
+	private double pointsPerMile;
 
 	public AwardTicket(String depAirportCode, String arrAirportCode, String depTime, String arrTime, String flightNo,
 			String flightClass, String aircraftType, String date, String seat, ArrayList<Person> ticketHolder,
 			String ticketNote, String productCode, String pointsPerMile) {
 		super(depAirportCode, arrAirportCode, depTime, arrTime, flightNo, flightClass, aircraftType, date, seat,
 				ticketHolder, ticketNote, productCode, "TA");
-		this.pointsPerMile = pointsPerMile;
+		this.pointsPerMile = Double.parseDouble(pointsPerMile);
 	}
 
 	public AwardTicket(String productCode, String date, String seat, ArrayList<Person> ticketHolders,
 			String ticketNote) {
 		super(productCode, "TA", date, seat, ticketHolders, ticketNote);
-		Scanner productFile = null;
+		
 		try {
-			productFile = new Scanner(new FileReader("data/Products.dat"));
-		} catch (FileNotFoundException e) {
-			System.out.println("Products.dat not found.");
-		}
-		while (productFile.hasNextLine()) {
-			String line = productFile.nextLine();
-			String[] productData = line.split(";");
-			if (productCode.equals(productData[0]) && "TA".equals(productData[1])) {
-				this.pointsPerMile = productData[9];
-			}
+			PreparedStatement ps = DatabaseInfo.getConnection()
+					.prepareStatement("SELECT ATPointsPerMile, "
+							+  "FROM Product WHERE ProductCode = ?");
+			ps.setString(1, this.getProductCode());
+			ResultSet rs = ps.executeQuery();
+			this.pointsPerMile = rs.getDouble("ATPointsPerMile");
+
+			ps.close();
+
+		} catch (SQLException e1) {
+			log.error("Failed to retrieve award ticket details under code " + productCode, e1);
 		}
 	}
 	
@@ -43,14 +46,13 @@ public class AwardTicket extends Ticket {
 		return 30.00;
 	}
 
-	public String getPointsPerMile() {
+	public double getPointsPerMile() {
 		return pointsPerMile;
 	}
 
 	@Override
 	public double calculatePrice() {
-		// TODO Auto-generated method stub
-		return 0;
+		return 30.00;
 	}
 
 }
