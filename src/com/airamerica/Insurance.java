d59de4a7ebb1503a77178cb6b48a03712531c5fe
rutebega <esper73@gmail.com>
2015-11-10 22:02:39 -0600

    Update Insurance.java

--- src/com/airamerica/Insurance.java
+++ src/com/airamerica/Insurance.java
@@ -4,23 +4,27 @@
  */
 package com.airamerica;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
+import com.airamerica.utils.DatabaseInfo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author EPiquette
  */
-public class Insurance extends Services {
+public class Insurance extends Service {
 
 	private String name;
 	private String ageClass;
-	private String costPerMile;
+	private double costPerMile;
 	private int quantity;
 	private String ticketCode;
 
-	public Insurance(String name, String ageClass, String costPerMile, int quantity, String ticketCode,
+	public Insurance(String name, String ageClass, double costPerMile, int quantity, String ticketCode,
 			String productCode) {
 		super(productCode, "SI");
 		this.name = name;
@@ -34,21 +38,27 @@ public class Insurance extends Services {
 		super(productCode, "SI");
 		this.quantity = quantity;
 		this.ticketCode = ticketCode;
-		Scanner productFile = null;
-		try {
-			productFile = new Scanner(new FileReader("data/Products.dat"));
-		} catch (FileNotFoundException e) {
-			System.out.println("Products.dat not found.");
-		}
-		while (productFile.hasNextLine()) {
-			String line = productFile.nextLine();
-			String[] productData = line.split(";");
-			if (productCode.equals(productData[0]) && "SI".equals(productData[1])) {
-				this.name = productData[2];
-				this.ageClass = productData[3];
-				this.costPerMile = productData[4];
-			}
-		}
+                
+                
+                Connection conn = DatabaseInfo.getConnection();
+                PreparedStatement ps;
+                ResultSet rs;
+                String getInsurance = "select ProductPrintName, InsuranceAgeClass, "
+                        + "InsuranceCostPerMile from Product where ProductCode = ?";
+            try {                
+                ps = conn.prepareStatement(getInsurance);
+                ps.setString(1, this.getProductCode());
+                rs = ps.executeQuery();
+                rs.next();
+                this.name = rs.getString("ProductPrintName");
+                this.ageClass = rs.getString("InsuranceAgeClass");
+                this.costPerMile = rs.getDouble("InsuranceCostPerMile");
+                rs.close();
+                ps.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Ticket.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
 	}
 
 	public String getName() {
@@ -59,16 +69,18 @@ public class Insurance extends Services {
 		return ageClass;
 	}
 
-	public String getCostPerMile() {
+	public double getCostPerMile() {
 		return costPerMile;
 	}
 	
-	    @Override
+
+        @Override
     public double calculatePrice() {
         return costPerMile*getMiles();
     }
 
- @Override
+
+        @Override
     public double calculateTax() {
         return calculatePrice()*0.04;
     }
