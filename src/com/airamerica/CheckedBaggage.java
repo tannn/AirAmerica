6def7c3b7bded87dd72ceabfc5eec7d6bd04f08b
Tanner <tannn@users.noreply.github.com>
2015-11-10 23:05:37 -0600

    Update CheckedBaggage.java

--- src/com/airamerica/CheckedBaggage.java
+++ src/com/airamerica/CheckedBaggage.java
@@ -1,48 +1,62 @@
 
 package com.airamerica;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Scanner;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.airamerica.utils.DatabaseInfo;
 
 /**
  *
  * @author EPiquette
  */
-public class CheckedBaggage extends Services {
+public class CheckedBaggage extends Service {
 
-    private String ticketCode;
-    private int quantity;
+	private String ticketCode;
+	private int quantity;
 
-    public CheckedBaggage(String productCode, String ticketCode, int quantity) {
-        super(productCode, "SC");
-        this.ticketCode = ticketCode;
-        this.quantity = quantity;
-    }
+	public CheckedBaggage(String productCode, String ticketCode, int quantity) {
+		super(productCode, "SC");
+		this.ticketCode = ticketCode;
+		this.quantity = quantity;
+	}
 
-    public CheckedBaggage(String productCode, int quantity) {
-        super(productCode, "SC");
-        this.quantity = quantity;
-        Scanner productFile = null;
-        try {
-		productFile = new Scanner(new FileReader("data/Products.dat"));
-            } catch (FileNotFoundException e) {
-		System.out.println("Produdcts.dat not found.");
-            }
-        while(productFile.hasNextLine()) {  
-                String line = productFile.nextLine();
-                String[] productData = line.split(";");
-                if(productCode.equals(productData[0])&&"SC".equals(productData[1])){
-                    this.ticketCode = productData[2];
-                }
-        }
-    }
+	public CheckedBaggage(String productCode, int quantity) {
+		super(productCode, "SC");
+		this.quantity = quantity;
 
-    public String getTicketCode() {
-        return ticketCode;
-    }
-    
-    public int getQuantity() {
-    	return quantity;
-    }
+		try {
+			PreparedStatement ps = DatabaseInfo.getConnection()
+					.prepareStatement("SELECT TicketCode FROM Product WHERE ProductCode = ?");
+			ps.setString(1, productCode);
+			ResultSet rs = ps.executeQuery();
+			this.ticketCode = rs.getString("TicketCode");
+			ps.close();
+
+		} catch (SQLException e1) {
+			log.error("Failed to retrieve checked baggage under code " + ticketCode, e1);
+		}
+	}
+
+	public String getTicketCode() {
+		return ticketCode;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	@Override
+	public double calculatePrice() {
+		if (quantity == 1)
+			return 25.00;
+		else
+			return 25.00 + ((quantity - 1) * 35.00);
+	}
+
+	@Override
+	public double calculateTax() {
+		return calculatePrice() * .04;
+	}
 }
