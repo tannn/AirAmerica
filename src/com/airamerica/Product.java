606252d7d450eeffcf9174650caba37ff45ea62d
rutebega <esper73@gmail.com>
2015-11-10 21:56:39 -0600

    Update Product.java

--- src/com/airamerica/Product.java
+++ src/com/airamerica/Product.java
@@ -1,18 +1,18 @@
 package com.airamerica;
 
+import com.airamerica.utils.DatabaseInfo;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import org.apache.log4j.Logger;
-
-import com.airamerica.interfaces.InvoiceData;
-import com.airamerica.utils.DatabaseInfo;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 abstract public class Product {
-
-	public static Logger log = Logger.getLogger(InvoiceData.class);
-
+	//calculate price, polymorphism
 	private String productCode;
 	private String productType;
 
@@ -30,28 +30,33 @@ abstract public class Product {
 	}
 
 	/**
-	 * @param code
-	 *            Code of product
-	 * @return Type of product
+	 * @param code Code of product
+	 * @return	Type of product
 	 */
 	public static String getProductType(String code) {
-
-		try {
-			PreparedStatement ps = DatabaseInfo.getConnection()
-					.prepareStatement("SELECT ProductType FROM Product WHERE ProductCode = ?");
-			ps.setString(1, code);
-			ResultSet rs = ps.executeQuery();
-			ps.close();
-			return rs.getString("ProductType");
-
-		} catch (SQLException e1) {
-			log.error("Failed to retrieve airport under code " + code, e1);
-		}
-
-		return null;
+		String type = null;
+                
+                
+                Connection conn = DatabaseInfo.getConnection();
+                PreparedStatement ps;
+                ResultSet rs;
+                String getType = "select ProductType from Product where ProductCode = ?";
+            try {                
+                ps = conn.prepareStatement(getType);
+                ps.setString(1, code);
+                rs = ps.executeQuery();
+                rs.next();
+                type = rs.getString("ProductType");
+                rs.close();
+                ps.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Ticket.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+		return type;
 	}
+        
+        abstract double calculateTax();
+        abstract double calculatePrice();
 
-	public abstract double calculatePrice();
-
-	public abstract double calculateTax();
 }
