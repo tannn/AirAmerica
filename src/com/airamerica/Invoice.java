a17ea27d83b85b59b65a5489755be54ec6bfdb86
rutebega <esper73@gmail.com>
2015-11-10 23:59:23 -0600

    Update Invoice.java

--- src/com/airamerica/Invoice.java
+++ src/com/airamerica/Invoice.java
@@ -1,6 +1,13 @@
 package com.airamerica;
 
+import com.airamerica.utils.DatabaseInfo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -8,124 +15,164 @@ import java.util.ArrayList;
  */
 public class Invoice {
 
-	private String invoiceCode;
-	private String customerCode;
-	private String salespersonCode;
-	private String invoiceDate;
-	private ArrayList<Product> products;
-	
+    private String invoiceCode;
+    private String customerCode;
+    private String salespersonCode;
+    private String invoiceDate;
+    private ArrayList<Product> products;
 
-	public Invoice(String invoiceCode, String customerCode, String salespersonCode, String invoiceDate,
-			String[] productInfo) {
-		this.invoiceCode = invoiceCode;
-		this.customerCode = customerCode;
-		this.salespersonCode = salespersonCode;
-		this.invoiceDate = invoiceDate;
-		products = new ArrayList<Product>();
-		ArrayList<Person> ticketHolders = new ArrayList<Person>();
-		
-		for (String s : productInfo) {
-			String[] productInvoice = s.split(":");
-			if (Product.getProductType(productInvoice[0].trim()).equals("TS")) {
-				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
-					ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
-							Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
-				}
-				products.add(new Ticket(productInvoice[0], "TS", productInvoice[1], productInvoice[3], ticketHolders,
-						productInvoice[productInvoice.length - 1]));
-			} else if (Product.getProductType(productInvoice[0]).equals("TA")) {
-				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
-					ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
-							Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
-				}
-				products.add(new AwardTicket(productInvoice[0], productInvoice[1], productInvoice[3],
-						ticketHolders, productInvoice[productInvoice.length - 1]));
-			} else if (Product.getProductType(productInvoice[0]).equals("TO")) {
-				for (int i = 0; i < Integer.parseInt(productInvoice[2]); i++) {
-					ticketHolders.add(new Person(productInvoice[4 + i * 5], productInvoice[5 + i * 5],
-							Integer.parseInt(productInvoice[6 + i * 5]), productInvoice[7 + i * 5]));
-				}
-				products.add(new OffseasonTicket(productInvoice[0], productInvoice[1],
-						productInvoice[3], ticketHolders, productInvoice[productInvoice.length - 1]));
-			} else if (Product.getProductType(productInvoice[0]).equals("SI")) {
-				products.add(new Insurance(productInvoice[0], Integer.parseInt(productInvoice[1]), productInvoice[2]));
-			} else if (Product.getProductType(productInvoice[0]).equals("SS")) {
-				products.add(new SpecialAssistance(productInvoice[0], productInvoice[1]));
-			} else if (Product.getProductType(productInvoice[0]).equals("SC")) {
-				products.add(new CheckedBaggage(productInvoice[0], Integer.parseInt(productInvoice[1])));
-			} else if (Product.getProductType(productInvoice[0]).equals("SR")) {
-				products.add(new Refreshment(productInvoice[0], Integer.parseInt(productInvoice[1])));
-			}
+    public Invoice(String invoiceCode, String customerCode, String salespersonCode, String invoiceDate,
+            ArrayList<Integer> productInfo) {
+        this.invoiceCode = invoiceCode;
+        this.customerCode = customerCode;
+        this.salespersonCode = salespersonCode;
+        this.invoiceDate = invoiceDate;
+        products = new ArrayList<>();
+        ArrayList<Person> ticketHolders = new ArrayList<>();
 
-		}
+        for (Integer i : productInfo) {
+            Connection conn = DatabaseInfo.getConnection();
+            PreparedStatement ps1;
+            PreparedStatement ps2;
+            ResultSet rs1;
+            ResultSet rs2;
+            String getProductInfo = "select * from Product join InvoiceProduct join Passenger"
+                    + "on Product.Product_ID = InvoiceProduct.Product_ID and "
+                    + "InvoiceProduct.Passenger_ID = Passenger.Passenger_ID"
+                    + "where Product_ID = ?";
 
-	}
+            String getPersonInfo = "select PersonCode, Age, Nationality from "
+                    + "Passenger join Person on Passenger.Person_ID = "
+                    + "Person.Person_ID where Product_ID = ?";
+            String productType = null;
+            try {
+                ps1 = conn.prepareStatement(getProductInfo);
+                ps1.setInt(1, i);
+                rs1 = ps1.executeQuery();
+                rs1.next();
+                productType = rs1.getString("ProductType");
 
-	public String getInvoiceCode() {
-		return invoiceCode;
-	}
+                ps2 = conn.prepareStatement(getPersonInfo);
+                ps2.setInt(1, i);
+                rs2 = ps2.executeQuery();
+                rs2.next();
 
-	public String getCustomerCode() {
-		return customerCode;
-	}
+                if (productType.equals("TS")) {
+                    while (!rs2.isLast()) {
+                        ticketHolders.add(new Person(rs2.getString("PersonCode"),
+                                rs2.getInt("Age"), rs2.getString("Nationality")));
+                        rs2.next();
+                    }
+                    products.add(new Ticket(rs1.getString("ProductCode"), "TS",
+                            rs1.getString("FlightDate"), rs1.getString("SeatNumber"),
+                            ticketHolders, rs1.getString("TicketNote")));
+                } else if (productType.equals("TA")) {
+                    while (!rs2.isLast()) {
+                        ticketHolders.add(new Person(rs2.getString("PersonCode"),
+                                rs2.getInt("Age"), rs2.getString("Nationality")));
+                        rs2.next();
+                    }
+                    products.add(new AwardTicket(rs1.getString("ProductCode"),
+                            rs1.getString("FlightDate"), rs1.getString("SeatNumber"),
+                            ticketHolders, rs1.getString("TicketNote")));
+                } else if (productType.equals("TO")) {
+                    while (!rs2.isLast()) {
+                        ticketHolders.add(new Person(rs2.getString("PersonCode"),
+                                rs2.getInt("Age"), rs2.getString("Nationality")));
+                        rs2.next();
+                    }
+                    products.add(new OffseasonTicket(rs1.getString("ProductCode"),
+                            rs1.getString("FlightDate"), rs1.getString("SeatNumber"),
+                            ticketHolders, rs1.getString("TicketNote")));
+                } else if (productType.equals("SI")) {
+                    products.add(new Insurance(rs1.getString("ProductCode"),rs1.getInt("Quantity"),rs1.getString("TicketCode")));
+                } else if (productType.equals("SS")) {
+                    products.add(new SpecialAssistance(rs1.getString("ProductCode"),rs1.getString("SATypeOfService")));
+                } else if (productType.equals("SC")) {
+                    products.add(new CheckedBaggage(rs1.getString("ProductCode"),rs1.getInt("Quantity")));
+                } else if (productType.equals("SR")) {
+                    products.add(new Refreshment(rs1.getString("ProductCode"),rs1.getInt("Quantity")));
+                }
 
-	public String getSalespersonCode() {
-		return salespersonCode;
-	}
+                rs2.close();
+                ps2.close();
+                rs1.close();
+                ps1.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Ticket.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            ticketHolders = new ArrayList<>();
+        }
 
-	/**
-	 *
-	 * @return Name (Last, First) of the salesperson
-	 */
-	public String getSalesperson() {
-		if (salespersonCode.equalsIgnoreCase("online")) {
-			return "ONLINE";
-		} else {
-			return Person.getPersonName(salespersonCode);
-		}
-	}
+    }
 
-	public String getInvoiceDate() {
-		return invoiceDate;
-	}
+    public String getInvoiceCode() {
+        return invoiceCode;
+    }
 
-	public ArrayList<Product> getProducts() {
-		return products;
-	}
-	
-	public double getSubtotals() {
-		float total = 0;
-		for (Product p : products) {
-			total += p.calculatePrice();
-		}
-		return total;
-	}
-	
-	public double getFees() {
-		if (Customer.getCustomerType(getCustomerCode()).equals("[CORPORATE]")) 
-			return 40.00;
-		else
-			return 0.00;
-	}
-	
-	public double getTaxes() {
-		float total = 0;
-		for (Product p : products) {
-			total += p.calculateTax();
-		}
-		return total;
-	}
-	
-	public double getDiscounts() {
-		if (Customer.getCustomerType(getCustomerCode()).equals("[CORPORATE]")) 
-			return getSubtotals()  * .12;
-		else if (Customer.getCustomerType(getCustomerCode()).equals("[GOVERNMENT]")) 
-			return getTaxes();
-		else
-			return 0;
-	}
-	
-	public double getTotals() {
-		return getSubtotals() + getFees() + getTaxes() - getDiscounts();
-	}
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public String getSalespersonCode() {
+        return salespersonCode;
+    }
+
+    /**
+     *
+     * @return Name (Last, First) of the salesperson
+     */
+    public String getSalesperson() {
+        if (salespersonCode.equalsIgnoreCase("online")) {
+            return "ONLINE";
+        } else {
+            return Person.getPersonName(salespersonCode);
+        }
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public double getSubtotals() {
+        float total = 0;
+        for (Product p : products) {
+            total += p.calculatePrice();
+        }
+        return total;
+    }
+
+    public double getFees() {
+        if (Customer.getCustomerType(getCustomerCode()).equals("[CORPORATE]")) {
+            return 40.00;
+        } else {
+            return 0.00;
+        }
+    }
+
+    public double getTaxes() {
+        float total = 0;
+        for (Product p : products) {
+            total += p.calculateTax();
+        }
+        return total;
+    }
+
+    public double getDiscounts() {
+        if (Customer.getCustomerType(getCustomerCode()).equals("[CORPORATE]")) {
+            return getSubtotals() * .12;
+        } else if (Customer.getCustomerType(getCustomerCode()).equals("[GOVERNMENT]")) {
+            return getTaxes();
+        } else {
+            return 0;
+        }
+    }
+
+    public double getTotals() {
+        return getSubtotals() + getFees() + getTaxes() - getDiscounts();
+    }
 }
